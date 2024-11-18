0e7b20233ffb996693c994ef7574986c64541ea1
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-11-18 23:06:10 +0100

    Create TicTacToeCpp.cpp
    Main file could be useful

---
+++ TicTacToeCpp/TicTacToeCpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "Include/Grid.hpp"
+#include "Include/Game.hpp"
+
+int main(int, char**)
+{
+    // std::cout << "Hello World!" << std::endl;
+
+    Game().Run();
+}
