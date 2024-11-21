13e9c14fb1f3d5b00b09134ef66677f4fe590023
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-11-21 22:11:56 +0100

    Update TicTacToeCpp.cpp

--- TicTacToeCpp/TicTacToeCpp.cpp
+++ TicTacToeCpp/TicTacToeCpp.cpp
@@ -2,10 +2,22 @@
 
 #include "Include/Grid.hpp"
 #include "Include/Game.hpp"
+#include "Include/Tree.hpp"
+
+#define RUN_GAME
 
 int main(int, char**)
 {
-    // std::cout << "Hello World!" << std::endl;
-
-    Game().Run();
+#ifdef RUN_GAME
+	Game(true, false).Run();
+#else
+	Node<int> node = Node<int>(1);
+	node.AddChild(new Node<int>(3));
+	node.AddChild(new Node<int>(2));
+	node.Children[0]->AddChild(new Node<int>(4));
+	node.Children[0]->AddChild(new Node<int>(5));
+	node.Children[1]->AddChild(new Node<int>(7));
+	node.Children[1]->AddChild(new Node<int>(6));
+	node.DepthFirst();
+#endif
 }
