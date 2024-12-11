28172bbd0f64e5e05d8d4849fff7844fa9f17b40
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-12-11 23:59:21 +0100

    Window

--- TicTacToeCpp/Include/Window.hpp
+++ TicTacToeCpp/Include/Window.hpp
@@ -11,6 +11,7 @@ public:
 	Window(int windowWidth, int windowHeight, const std::string& windowName);
 	~Window();
 	sf::RenderWindow* RenderWindow = nullptr;
-	void Rename(const std::string& newName);
+	void Rename(const std::string& newName); // make it flip between tic & tac and toe when sb wins
 private:
+	
 };
---
+++ TicTacToeCpp/Source/Window.cpp
@@ -0,0 +1,17 @@
+#include "Window.hpp"
+#include "SpritesData.hpp"
+
+Window::Window(int windowWidth, int windowHeight, const std::string& windowName)
+{
+	RenderWindow = new sf::RenderWindow(sf::VideoMode(windowWidth, windowHeight), windowName);
+}
+
+Window::~Window()
+{
+	delete RenderWindow;
+}
+
+void Window::Rename(const std::string& newName)
+{
+	RenderWindow->setTitle(newName);
+}
