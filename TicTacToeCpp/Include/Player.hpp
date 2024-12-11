6ffdd53e0adffc497a81baa576f2e929046f8a3f
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2024-12-11 23:59:52 +0100

    Update Player.hpp

--- TicTacToeCpp/Include/Player.hpp
+++ TicTacToeCpp/Include/Player.hpp
@@ -4,13 +4,16 @@
 
 #include "Grid.hpp"
 
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
 
 class Player
 {
 public:
 	Player() = default;
 	Player(char symbol);
-	virtual int GatherInput(std::function<bool(int)>inputPredicate, Grid& grid) = 0;
+	virtual int GatherInput(std::function<bool(int)>inputPredicate, Grid& grid, sf::RenderWindow* renderWindow) = 0;
 	virtual void Reset();
 	char Symbol;
 };
