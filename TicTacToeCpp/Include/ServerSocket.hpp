ea3a48d4bae36a1fb472959e1cbdb1ee8c4fc7b9
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2025-01-26 00:00:06 +0100

    somewhat handle several packet types

--- TicTacToeCpp/Include/ServerSocket.hpp
+++ TicTacToeCpp/Include/ServerSocket.hpp
@@ -15,7 +15,7 @@ public:
     }
     void Run() override
     {
-        char buffer[BUFFER_SIZE];
+        char buffer[CHAT_BUFFER_SIZE];
 
         while (true)
         {
@@ -29,7 +29,7 @@ public:
                 continue;
             }
 
-            if (recv(clientSocket, buffer, BUFFER_SIZE, 0) <= 0)
+            if (recv(clientSocket, buffer, CHAT_BUFFER_SIZE, 0) <= 0)
             {
                 std::cerr << "Gone too soon" << std::endl;
                 continue;
@@ -57,7 +57,11 @@ public:
 
         Cleanup();
     }
-
+    int GetConnectedClientsCount() const
+    {
+        return connectedClients;
+    }
+    
 private:
     void Init()
     {
@@ -85,43 +89,79 @@ private:
     std::vector<std::thread> clientThreads;
     std::vector<SOCKET> clientSockets;
     int connectedClients = 0;
+
     void HandleClient(SOCKET clientSocket, const std::string& name, int clientNumber)
     {
-        char buffer[BUFFER_SIZE];
-
-        char message[BUFFER_SIZE];
+        char headerBuffer[sizeof(Header)];
 
         int nameLength = (int)name.length() + 3; // + 3 ->account for :<space> + null terminator
         while (true)
         {
-            int bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+            int bytesReceived = recv(clientSocket, headerBuffer, sizeof(Header), 0);
             if (bytesReceived <= 0)
             {
                 std::cout << name  << " disconnected" << std::endl;
                 connectedClients--;
+
                 break;
             }
 
-            std::cout << name  << ": " << buffer << std::endl;
-
-            _ = strcpy_s(message, name.c_str());
-            _ = strcat_s(message, ": ");
-            _ = strcat_s(message, buffer);
-
-            if (connectedClients < 2)
+            switch ((SerializationHeaders)headerBuffer[0])
             {
-                continue; // why even bother you are lonely AF just give up
-            }
+                case SerializationHeaders::ChatMessage:
+                    HandleChatMessage(clientSocket, name, clientNumber);
+                    break;
 
-            if (bytesReceived + nameLength > BUFFER_SIZE)
-            {
-                 // account for this at some point
+                case SerializationHeaders::Play:
+                    HandlePlay(clientSocket, clientNumber);
+                    break;
             }
-
-            _ = send(clientSockets[(int)(!(bool)clientNumber)], message, BUFFER_SIZE, 0); // lowkey sinning
 
         }
 
         _ = closesocket(clientSocket);
+    }
+
+    void HandleChatMessage(SOCKET& clientSocket, const std::string& name, int clientNumber)
+    {
+        char message[CHAT_BUFFER_SIZE];
+        char chatBuffer[CHAT_BUFFER_SIZE];
+
+        _ = recv(clientSocket, chatBuffer, CHAT_BUFFER_SIZE, 0);
+
+        std::cout << name << ": " << chatBuffer << std::endl;
+
+        _ = strcpy_s(message, name.c_str());
+        _ = strcat_s(message, ": ");
+        _ = strcat_s(message, chatBuffer);
+
+        if (connectedClients < 2)
+        {
+            return; // why even bother you are lonely AF just give up
+        }
+
+        //if (bytesReceived + nameLength > CHAT_BUFFER_SIZE)
+        //{
+        //    // account for this at some point
+        //}
+
+        _ = send(clientSockets[(int)(!(bool)clientNumber)], message, CHAT_BUFFER_SIZE, 0); // lowkey sinning
+    }
+
+    void HandlePlay(SOCKET& clientSocket, int clientNumber)
+    {
+        char playBuffer[sizeof(int)];
+
+        _ = recv(clientSocket, playBuffer, sizeof(int), 0);
+
+        int playValidity = PlayIsValid(*reinterpret_cast<int*>(playBuffer)) ? 9 : 10;
+
+        memcpy(playBuffer, &playValidity, sizeof(int));
+        Send(playBuffer, SerializationHeaders::Play);
+    }
+
+    bool PlayIsValid(int play)
+    {
+        return false;
     }
 };
