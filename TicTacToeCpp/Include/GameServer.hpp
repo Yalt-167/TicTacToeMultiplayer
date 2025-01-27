f38eb981f173887f61fa2c72cc7c10e530ebb0f7
Antoine Riviale <107402939+Yalt-167@users.noreply.github.com>
2025-01-27 15:54:50 +0100

    Update GameServer.hpp

--- TicTacToeCpp/Include/GameServer.hpp
+++ TicTacToeCpp/Include/GameServer.hpp
@@ -1,2 +1,28 @@
 #pragma once
 
+#include "Grid.hpp"
+#include "ServerSocket.hpp"
+
+
+class GameServer : public Game_
+{
+public:
+	GameServer()
+	{
+
+	}
+
+	void Run() override
+	{
+		serverSocket.Run();
+
+		while (serverSocket.GetConnectedClientsCount() < 2); // await players
+
+		//serverSocket.Broadcast(SerializationHeaders::Start.c_str());
+
+	}
+private:
+	Grid gameGrid;
+	ServerSocket serverSocket;
+	int playerTurn = 0;
+};
