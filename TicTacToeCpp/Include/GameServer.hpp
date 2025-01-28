e246e2e73e051529f75ea3b895616d12b21dd54a
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2025-01-28 23:54:43 +0100

    Update GameServer.hpp

--- TicTacToeCpp/Include/GameServer.hpp
+++ TicTacToeCpp/Include/GameServer.hpp
@@ -9,20 +9,41 @@ class GameServer : public Game_
 public:
 	GameServer()
 	{
-
+		Instance = this;
+		gameGrid = new Grid(true);
+	}
+	GameServer()
+	{
+		connectionThread->join();
+		delete connectionThread;
 	}
 
 	void Run() override
 	{
-		serverSocket.Run();
+		connectionThread = new std::thread(&ServerSocket::Run, serverSocket);
 
 		while (serverSocket.GetConnectedClientsCount() < 2); // await players
 
-		//serverSocket.Broadcast(SerializationHeaders::Start.c_str());
+		int startupPacket[sizeof(int) * 3]
+		{
+			(int)GameResult::None,
+			(int)Plays::InvalidPlay,
+			true,
+		};
 
+		serverSocket.Send(
+			reinterpret_cast<char*>(startupPacket),
+			SerializationHeaders::PlayResult, sizeof(int) * 3,
+			PacketSendTarget::Client0
+		);
 	}
+	int PlayerTurn = 0;
+	static GameServer* Instance;
 private:
-	Grid gameGrid;
+	Grid* gameGrid;
 	ServerSocket serverSocket;
-	int playerTurn = 0;
+	std::thread* connectionThread = nullptr;
 };
+
+
+GameServer* GameServer::Instance = nullptr;
