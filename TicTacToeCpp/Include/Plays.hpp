31bb44f8022a37486696487a6bd12862f2a3bbf3
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2025-01-29 02:18:31 +0100

    Create Plays.hpp

---
+++ TicTacToeCpp/Include/Plays.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+enum class Plays // name is dogshit but couldn t figure out a decent one so if u got one feel free ^^
+{
+	// one through eight are simply the slots
+	InvalidPlay = 9,
+};
